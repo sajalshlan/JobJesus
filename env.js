52dc912f4c8e6ec1cd668b84c71bc1fe69332159
Sajal Sharma <sajalsharma2408@gmail.com>
2023-04-02 21:47:21 +0530

    speech to text added

---
+++ env.js
@@ -0,0 +1,3 @@
+export const SPEECH_KEY = "9be35ba8e82648e794a86337bb16291c";
+export const SPEECH_REGION = "centralindia";
+export const DEEPGRAM_API_KEY = "1fe5acf1c4634111833d70394c01cf30cf386ef6";
