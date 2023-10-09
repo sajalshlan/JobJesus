5214f7f1d9f422074ab1d937d2f6de2843021b0c
Sajal Sharma <sajalsharma2408@gmail.com>
2023-10-09 23:13:36 +0530

    Update useOpenAI.jsx

--- src/components/useOpenAI.jsx
+++ src/components/useOpenAI.jsx
@@ -39,3 +39,65 @@ const useOpenAI = (apiKey) => {
 };
 
 export default useOpenAI;
+
+
+//
+
+import { useState, useEffect } from "react";
+import { Configuration, OpenAIApi } from "openai";
+
+function useOpenAI(apiKey) {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [openaiClient, setOpenaiClient] = useState(null);
+
+  useEffect(() => {
+    const configureOpenaiClient = async () => {
+      setLoading(true);
+      setError(null);
+
+      try {
+        const configuration = new Configuration({
+          apiKey: apiKey,
+        });
+        const client = new OpenAIApi(configuration);
+        setOpenaiClient(client);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    configureOpenaiClient();
+  }, [apiKey]);
+
+  const getCompletion = async (prompt, options) => {
+    if (!openaiClient) {
+      // Handle case where the client is not yet initialized.
+      return null;
+    }
+
+    setLoading(true);
+    setError(null);
+
+    try {
+      const response = await openaiClient.createChatCompletion({
+        model: "gpt-3.5-turbo",
+        messages: prompt,
+        ...options,
+      });
+      setLoading(false);
+      return response.data.choices[0].message;
+    } catch (error) {
+      setLoading(false);
+      setError(error);
+      return null;
+    }
+  };
+
+  return [getCompletion, loading, error];
+}
+
+export default useOpenAI;
+
